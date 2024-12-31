e24c840814def08616cc6ee1df3632fae2b9f954
amjed shakshuki <amjedshak@gmail.com>
2024-12-31 10:22:02 -0800

    src fixes 2

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'out',
   images: {
     unoptimized: true,
   },
-  assetPrefix: 'https://casa-de-arte.vercel.app',
   basePath: '',
+  assetPrefix: 'https://casa-de-arte.vercel.app',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig 
