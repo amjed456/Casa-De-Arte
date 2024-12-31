a357db56298759a1b4c7896146cae16064a1c338
amjed shakshuki <amjedshak@gmail.com>
2024-12-31 11:50:32 -0800

    last try i swear

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProduction = process.env.NODE_ENV === 'production'
-const repositoryName = 'Casa-De-Arte'
-
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  // Configure base path and asset prefix for GitHub Pages
-  
+  assetPrefix: '/',
   trailingSlash: true,
 }
 
