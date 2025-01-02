efefb5607573fa83fcffbf54dc00dc7215649b31
amjed shakshuki <amjedshak@gmail.com>
2025-01-02 05:35:05 -0800

    trial and error 1

--- next.config.js
+++ next.config.js
@@ -3,15 +3,9 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'casa-de-arte.vercel.app'
-      }
-    ]
   },
+  basePath: '',
   assetPrefix: '/',
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
