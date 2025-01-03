0338b339c21c667331b72b056b44f7b1e538abf1
amjed shakshuki <amjedshak@gmail.com>
2025-01-02 16:42:28 -0800

    Fixed asset paths7

--- next.config.js
+++ next.config.js
@@ -5,8 +5,19 @@ const nextConfig = {
     unoptimized: true,
   },
   basePath: '/casa-de-arte',
-  assetPrefix: 'https://amjed456.github.io/casa-de-arte',
+  assetPrefix: '/casa-de-arte/',
   trailingSlash: true,
+  headers: async () => [
+    {
+      source: '/:path*',
+      headers: [
+        {
+          key: 'Access-Control-Allow-Origin',
+          value: '*'
+        }
+      ]
+    }
+  ]
 }
 
 module.exports = nextConfig
