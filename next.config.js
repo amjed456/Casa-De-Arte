f0c4beaab1239dd94fbab5268974f7f93a68c807
amjed shakshuki <amjedshak@gmail.com>
2025-01-02 17:30:44 -0800

    Fixed asset paths12

--- next.config.js
+++ next.config.js
@@ -5,8 +5,7 @@ const nextConfig = {
     unoptimized: true,
   },
   basePath: '/casa-de-arte',
-  //assetPrefix: 'https://amjed456.github.io/casa-de-arte',
-  assetPrefix: '/casa-de-arte',
+  assetPrefix: 'https://amjed456.github.io/casa-de-arte', 
   trailingSlash: true,
   experimental: {
     optimizeFonts: true,
